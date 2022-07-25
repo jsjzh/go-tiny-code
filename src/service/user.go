53e08c52ed8e72b6192afee878e273b0646bbd66
金哲豪 <jinzhehao@souche.com>
2022-07-25 22:06:27 +0800

    modified

--- src/service/user.go
+++ src/service/user.go
@@ -1,8 +1,21 @@
 package service
 
-import "go-tiny-code/src/model"
+import (
+	"go-tiny-code/src/model"
+	"log"
+)
 
 func CreateUser() {
+
+	user := model.User{
+		Name:  "king",
+		Email: "kimimi_king@163.com",
+		Phone: "18368094601",
+	}
+
+	if error := model.DB().Create(&user).Error; error != nil {
+		log.Println(error)
+	}
 
 }
 
@@ -15,6 +28,12 @@ func UpdateUser() {
 }
 
 func ReadUser(query model.User) {
+	user := map[string]interface{}{}
+
+	model.DB().Take(&user)
+
+	log.Println("user", user)
+
 	// var user model.User
 
 	// model.DB().Take(query)
